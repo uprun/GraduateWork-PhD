54b382c0dc5e68a606b159e5816213ec5cae5586
uprun <uprun@uprun-Ubuntu-15.04>
2015-12-08 00:19:31 +0200

    Added extraction of gradient points

--- Neuron_mod.py
+++ Neuron_mod.py
@@ -6,15 +6,35 @@ from os import listdir
 from os.path import isfile, join
 from matplotlib import pyplot as plt
 
+def getGradientPoints(image):
+    img_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    dx = cv2.Sobel(img_grey, cv2.CV_16S, 1, 0)
+    abs_dx = cv2.convertScaleAbs( dx)
+    dy = cv2.Sobel(img_grey, cv2.CV_16S, 0, 1)
+    abs_dy = cv2.convertScaleAbs( dy)
+    grad_approx = cv2.addWeighted( abs_dx, 0.5, abs_dy, 0.5, 0 )
+    cv2.namedWindow("gr", cv2.CV_WINDOW_AUTOSIZE)
+    cv2.imshow("gr",grad_approx)
+    cv2.waitKey(0) & 0xFF
+    cv2.destroyWindow("gr")
+    averageValue = numpyLib.average(grad_approx)
+    nonZeroPoints = numpyLib.nonzero(grad_approx > averageValue * 0.5)
 
+    return [(a[1], a[0]) for a in zip(*nonZeroPoints)]
 
 def getPointsFromImage(fileName, size_of_ann):
-    img = cv2.imread(fileName, 0)
+    img = cv2.imread(fileName, cv2.CV_LOAD_IMAGE_COLOR)
+    cv2.namedWindow("temp", cv2.CV_WINDOW_AUTOSIZE)
+    gradientPoints = getGradientPoints(img)
     # Initiate STAR detector
     orb = cv2.ORB(1800)
 
     # find the keypoints with ORB
     kp = orb.detect(img,None)
+
+    orbPoints = [x.pt for x in kp]
+
+    mergedPoints = orbPoints + gradientPoints
 
     # compute the descriptors with ORB
     #kp, des = orb.compute(img, kp)
@@ -24,24 +44,24 @@ def getPointsFromImage(fileName, size_of_ann):
     min_y = 0
     max_x = 0
     max_y = 0
-    if len(kp) > 0:
+    if len(mergedPoints) > 0:
         mid_x = 0
         mid_y = 0
-        min_x = kp[0].pt[0]
-        min_y = kp[0].pt[1]
+        min_x = mergedPoints[0][0]
+        min_y = mergedPoints[0][1]
         max_x = min_x
         max_y = min_y
-        for point in kp:
-            x = point.pt[0]
-            y = point.pt[1]
+        for point in mergedPoints:
+            x = point[0]
+            y = point[1]
             mid_x += x
             mid_y += y
             min_x = min(min_x, x)
             min_y = min(min_y, y)
             max_x = max(max_x, x)
             max_y = max(max_y, y)
-        mid_x /= len(kp)
-        mid_y /= len(kp)
+        mid_x /= len(mergedPoints)
+        mid_y /= len(mergedPoints)
         print mid_x, ":",  mid_y
     max_dist_from_center = max(mid_x - min_x, max_x - mid_x, mid_y - min_y, max_y - mid_y)
     ratio = size_of_ann / (2 * max_dist_from_center)
@@ -51,14 +71,14 @@ def getPointsFromImage(fileName, size_of_ann):
     max_y_i = math.floor(max_y).__int__()
     print min_x, min_y, max_x, max_y
     # draw only keypoints location,not size and orientation
-    img2 = cv2.drawKeypoints(img, kp, color=(255, 0, 0), flags=0)
+    #img2 = cv2.drawKeypoints(img, mergedPoints, color=(255, 0, 0), flags=0)
     cv2.line(img2, (min_x_i, min_y_i), (min_x_i, max_y_i), (0, 255, 0), 4)
     cv2.line(img2, (min_x_i, min_y_i), (max_x_i, min_y_i), (0, 255, 0), 4)
     cv2.line(img2, (max_x_i, max_y_i), (max_x_i, min_y_i), (0, 255, 0), 4)
     cv2.line(img2, (min_x_i, max_y_i), (max_x_i, max_y_i), (0, 255, 0), 4)
-    plt.imshow(img2), plt.show()
 
     result_vector = numpyLib.zeros((size_of_ann * size_of_ann, 1))
+
 
     for point in kp:
         x = point.pt[0]
@@ -74,13 +94,18 @@ def getPointsFromImage(fileName, size_of_ann):
         index = min(index, size_of_ann * size_of_ann)
         index = max(index, 0)
         result_vector[index] = 1
-    plt.imshow(img2), plt.show()
+    #plt.imshow(img2), plt.show()
+    cv2.imshow("temp",img2)
+    cv2.waitKey(0) & 0xFF
+    cv2.destroyAllWindows()
     return result_vector
 
 
 imagesNames = [f for f in listdir("./") if isfile(join("./", f)) and
     (f.endswith(".png") or f.endswith(".jpg"))]
-learningImages = [f for f in imagesNames if f.startswith("success_") or f.startswith("failure_")]
+learningImages = [f for f in imagesNames
+                    if f.startswith("success_") or
+                         f.startswith("failure_")]
 
 print imagesNames
 
