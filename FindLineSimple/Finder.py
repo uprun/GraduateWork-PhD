f4a36b11fbabefbd2bb76ec60c8f2ad198530774
uprun <geraclion@gmail.com>
2016-02-29 22:41:35 +0200

    Reorgranized code, this should slightly improve performance

--- FindLineSimple/Finder.py
+++ FindLineSimple/Finder.py
@@ -3,16 +3,15 @@ import cv2
 import math
 from os import listdir
 from os.path import isfile, join
-from matplotlib import pyplot as plt
 
 
-def getGradientPoints(image):
+def getGradientPointsFromImage(image):
     img_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     dx = cv2.Sobel(img_grey, cv2.CV_16S, 1, 0)
-    abs_dx = cv2.convertScaleAbs( dx)
+    abs_dx = cv2.convertScaleAbs(dx)
     dy = cv2.Sobel(img_grey, cv2.CV_16S, 0, 1)
-    abs_dy = cv2.convertScaleAbs( dy)
-    grad_approx = cv2.addWeighted( abs_dx, 0.5, abs_dy, 0.5, 0 )
+    abs_dy = cv2.convertScaleAbs(dy)
+    grad_approx = cv2.addWeighted(abs_dx, 0.5, abs_dy, 0.5, 0)
     #cv2.namedWindow("gradient", cv2.CV_WINDOW_AUTOSIZE)
     #cv2.imshow("gradient",grad_approx)
     #cv2.waitKey(0) & 0xFF
@@ -22,22 +21,29 @@ def getGradientPoints(image):
 
     return [(a[1], a[0]) for a in zip(*nonZeroPoints)]
 
-def getPointsFromImage(image, size_of_ann, rotation_degrees = 0, verbose = False):
+
+def getPoints(image, getGradientPoints=True, getORBKeyPoints=True):
+    img = image
+    mergedPoints = []
+    if getGradientPoints:
+        gradientPoints = getGradientPointsFromImage(img)
+        mergedPoints = gradientPoints + mergedPoints
+    if getORBKeyPoints:
+        # Initiate STAR detector
+        orb = cv2.ORB(1800)
+        # find the keypoints with ORB
+        keypoints = orb.detect(img, None)
+        orbPoints = [x.pt for x in keypoints]
+        # compute the descriptors with ORB
+        #kp, des = orb.compute(img, kp)
+        mergedPoints = orbPoints + mergedPoints
+    return mergedPoints
+
+
+def getPointsForAnn(image, size_of_ann, verbose=False):
     img = image
 
-    gradientPoints = getGradientPoints(img)
-    # Initiate STAR detector
-    orb = cv2.ORB(1800)
-
-    # find the keypoints with ORB
-    keypoints = orb.detect(img,None)
-
-    orbPoints = [x.pt for x in keypoints]
-
-    mergedPoints = orbPoints + gradientPoints
-
-    # compute the descriptors with ORB
-    #kp, des = orb.compute(img, kp)
+    mergedPoints = getPoints(img)
     mid_x = 0
     mid_y = 0
     min_x = 0
@@ -68,6 +74,10 @@ def getPointsFromImage(image, size_of_ann, rotation_degrees = 0, verbose = False
         mid_y /= len(mergedPoints)
         max_dist_from_center = max(mid_x - min_x, max_x - mid_x, mid_y - min_y, max_y - mid_y)
         ratio = size_of_ann / (2.0 * max_dist_from_center)
+
+    result_vector = numpyLib.zeros((size_of_ann, size_of_ann))
+    img2 = None
+    if verbose:
         min_x_i = math.floor(min_x).__int__()
         min_y_i = math.floor(min_y).__int__()
         max_x_i = math.floor(max_x).__int__()
@@ -78,10 +88,6 @@ def getPointsFromImage(image, size_of_ann, rotation_degrees = 0, verbose = False
         cv2.line(img2, (min_x_i, min_y_i), (max_x_i, min_y_i), (0, 255, 0), 4)
         cv2.line(img2, (max_x_i, max_y_i), (max_x_i, min_y_i), (0, 255, 0), 4)
         cv2.line(img2, (min_x_i, max_y_i), (max_x_i, max_y_i), (0, 255, 0), 4)
-
-    result_vector = numpyLib.zeros((size_of_ann , size_of_ann))
-
-
 
     for point in mergedPoints:
         x = point[0]
@@ -98,31 +104,37 @@ def getPointsFromImage(image, size_of_ann, rotation_degrees = 0, verbose = False
         iy = min(size_of_ann - 1, iy)
         ix = max(0, ix)
         iy = max(0, iy)
-        cv2.circle(img2, (ix, iy), 1, (0, 255, 0))
-        cv2.circle(
-             img2,
-             (point[0].__int__(), point[1].__int__()),
-             1,
-             (255, 0, 0))
-        #index = math.floor(y * size_of_ann + x).__int__()
-        #index = min(index, size_of_ann * size_of_ann)
-        #index = max(index, 0)
+        if verbose:
+            cv2.circle(img2, (ix, iy), 1, (0, 255, 0))
+            cv2.circle(
+                 img2,
+                 (point[0].__int__(), point[1].__int__()),
+                 1,
+                 (255, 0, 0))
         result_vector[iy, ix] = 1
-    if verbose :
+    if verbose:
         cv2.namedWindow("FoundKeyPoints", cv2.CV_WINDOW_AUTOSIZE)
         cv2.imshow("FoundKeyPoints", img2)
         cv2.waitKey(0) & 0xFF
+    return result_vector
+
+
+def applyANN(size_of_ann,
+         ann_net,
+         result_vector,
+         rotation_degrees=0,
+         verbose=False):
     cols = size_of_ann
     rows = size_of_ann
-    M = cv2.getRotationMatrix2D((cols / 2, rows / 2),rotation_degrees,1)
-    dst = cv2.warpAffine(result_vector,M,(cols,rows))
-    result_vector = dst.copy()
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), rotation_degrees, 1)
+    temp_vector = cv2.warpAffine(result_vector, M, (cols, rows))
     if verbose:
-        cv2.imshow("FoundKeyPoints", result_vector)
+        cv2.imshow("FoundKeyPoints", temp_vector)
         cv2.waitKey(0) & 0xFF
         cv2.destroyWindow("FoundKeyPoints")
-    return result_vector.flatten('C')
-
+    return ann_net(temp_vector.
+        flatten('C').
+        reshape(1, size_of_ann * size_of_ann))
 
 def shiftResultLocation(result, delta_x, delta_y):
     (a, b, (x, y), c) = result
@@ -133,9 +145,9 @@ def analyzeImage(image, size_of_ann, ann_net, verbose = False):
     (rows, columns, _ ) = image.shape
     print "Image shape: ",  image.shape
     results = []
+    points = getPointsForAnn(image, size_of_ann)
     for degree in range(0, 360, 4) :
-        res = ann_net(
-            getPointsFromImage(image, size_of_ann, degree).reshape(1, size_of_ann * size_of_ann))
+        res = applyANN(size_of_ann, ann_net, points, degree)
         results.append((res, degree))
         print imagePath," result: ", res, " degree: ",degree
     maxResult = max(results,key=lambda (result,_): result)
